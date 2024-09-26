4fa546519b8564933adf3b453d1baf84c8cea0d4
Thabang <thabangphiri07@gmail.com>
2024-09-26 13:17:06 +0200

    first commit

---
+++ src/state/Pages/App.jsx
@@ -0,0 +1,28 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Cakes from './Cakes.jsx'
+import Card from './Card.jsx'
+import ItemCard from './ItemCard.js';
+import TestComponent from './state/TestComponent';
+
+
+
+
+
+
+
+function App() {
+  
+  return(
+    <>
+    <Cakes/>
+    <Card/>
+    <ItemCard/>
+    <Footer/>
+    <Header/>
+    <TestComponent/>
+    </>
+  );
+}
+
+export default App
